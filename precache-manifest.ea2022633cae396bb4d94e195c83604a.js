01d83510ef4878e4ce5044a877fa11a56eb2c782
Eshwar S M <eshwar@eshwar.kali>
2021-02-02 11:56:19 +0530

    Updates

---
+++ precache-manifest.ea2022633cae396bb4d94e195c83604a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62834d8d79fe1848d339449a50268d4",
+    "url": "/thebridge/index.html"
+  },
+  {
+    "revision": "6f6b08869a1a1cf7f1c1",
+    "url": "/thebridge/static/css/main.c26d193d.chunk.css"
+  },
+  {
+    "revision": "c5da3cc8242677f9d034",
+    "url": "/thebridge/static/js/2.b374598e.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/thebridge/static/js/2.b374598e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f6b08869a1a1cf7f1c1",
+    "url": "/thebridge/static/js/main.733317b4.chunk.js"
+  },
+  {
+    "revision": "1d85447a3e661e9906ad",
+    "url": "/thebridge/static/js/runtime-main.7b5b9171.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/thebridge/static/media/cta-illustration.d41d8cd9.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/thebridge/static/media/illustration-section-01.d41d8cd9.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/thebridge/static/media/illustration-section-02.d41d8cd9.svg"
+  },
+  {
+    "revision": "632d447d8f74dd1f6e55d79039dc7138",
+    "url": "/thebridge/static/media/logo.632d447d.png"
+  }
+]);
